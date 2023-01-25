5a2a0a6969705c4ec700259c94325cb11bfa5e21
Igor Guimarães <igorgui.tec@gmail.com>
2023-01-25 20:18:24 -0300

    Esqueleto

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./Header";
+
+export default function App() {
+    return (
+        <>
+            <Header />
+            <BrowserRouter>
+            </BrowserRouter>
+        </>
+    )
+}
